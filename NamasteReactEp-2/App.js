e4aad9a58a7dd768f7eaa5c4cb760c15c1e42ec3
adityag28 <adityag0028@gmail.com>
2025-02-20 13:21:17 +0530

    Update

--- NamasteReactEp-2/App.js
+++ NamasteReactEp-2/App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
+    React.createElement("h2", {}, "I'm mad tag"),
   ]),
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I'm h1 tag"),
